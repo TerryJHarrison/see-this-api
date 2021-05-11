0cd66432fcfebc32356b7e563873ee38d3698cad
TJ Harrison <tjharrisonjr@gmail.com>
2021-05-10 23:09:25 -0400

    Generate random path when none is provided, include in response

--- src/createShortLink/lambda_function.py
+++ src/createShortLink/lambda_function.py
@@ -1,16 +1,14 @@
 import boto3
 import time
 import botocore
+from random_word import RandomWords
 
-dynamodb = None
+table = None
+random = None
 
 
 def put_link(link, url):
-    global dynamodb
-    if not dynamodb:
-        dynamodb = boto3.resource('dynamodb')
-
-    table = dynamodb.Table('st-short-links')
+    global table
     # expires in 7 days
     response = table.put_item(
        Item={
@@ -23,12 +21,43 @@ def put_link(link, url):
     return response
 
 
+def does_link_exist(link):
+    global table
+    response = table.get_item(
+        Key={
+            'link': link
+        }
+    )
+    return True if 'Item' in response else False
+
+
+def generate_random_path():
+    global random
+    if not random:
+        random = RandomWords()
+
+    verb = random.get_random_words(maxLength=5, includePartOfSpeech="verb", limit=1)[0]
+    noun = random.get_random_words(maxLength=5, includePartOfSpeech="noun", limit=1)[0]
+    path = f"{verb}-{noun}"
+    # Recurse if necessary until a new link is generated
+    return generate_random_path() if does_link_exist(path) else path
+
+
 def lambda_handler(event, context):
+    global table
+    if not table:
+        dynamodb = boto3.resource('dynamodb')
+        table = dynamodb.Table('st-short-links')
+
+    # Use provided path or generate one otherwise
+    path = event["link"] if "link" not in event or event["link"] is None else generate_random_path()
+
     try:
-        put_link(event["link"], event["url"])
+        put_link(path, event["url"])
         return {
             'statusCode': 200,
-            'message': "Short link added"
+            'message': "Short link added",
+            'link': path
         }
     except botocore.exceptions.ClientError as e:
         if e.response['Error']['Code'] == 'ConditionalCheckFailedException':
