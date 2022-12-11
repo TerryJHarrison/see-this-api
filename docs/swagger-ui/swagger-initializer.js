4625a072b5cfa765503518b6c0fae8a3569dc0da
TJ Harrison <tjharrisonjr@gmail.com>
2022-12-11 08:23:27 -0500

    Switch swagger file path based off environment temp fix

--- docs/swagger-ui/swagger-initializer.js
+++ docs/swagger-ui/swagger-initializer.js
@@ -1,9 +1,14 @@
 window.onload = function() {
     //<editor-fold desc="Changeable Configuration Block">
 
+    // Set path to swagger file base off environment
+    // TODO: more elegant solution
+    const isLocal = false;
+    const url = isLocal ? "/swagger.json" : "/see-this-api/swagger.json";
+
     // the following lines will be replaced by docker/configurator, when it runs in a docker-container
     window.ui = SwaggerUIBundle({
-        url: "/see-this-api/swagger.json",
+        url: url,
         dom_id: '#swagger-ui',
         deepLinking: true,
         presets: [
